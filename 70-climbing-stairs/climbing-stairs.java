9e814eadeb50eba12da2a5a6912d5673fa1b2891
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-03-17 17:06:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (41.42%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -5,8 +5,11 @@ class Solution {
     }
 
     static int dpApp(int n, int[] dp){
-        if(n == 1 || n == 2){
-            return n;
+        if(n == 0){
+            return 1;
+        }
+        if(n < 0){
+            return 0;
         }
 
         if(dp[n] != 0){
