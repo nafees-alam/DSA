c8a9dc19a2ba488e0297c1051010a04198ac3e12
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-05-28 17:32:19 +0530

    Time: 2749 ms (5.03%) | Memory: 40 MB (69.70%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -4,18 +4,12 @@ class Solution {
             return 1;
         }
 
-        int[] dp = new int[n+1];
-        dp[0] = dp[1] = 1;
-        if(dp[n] != 0){
-            return dp[n];
-        }
-
         int ans = 0;
         for(int i = 0; i < n; i++){
             ans += numTrees(i) * numTrees(n-i-1);
         }
-        dp[n] = ans;
+        
 
-        return dp[n];
+        return ans;
     }
 }
