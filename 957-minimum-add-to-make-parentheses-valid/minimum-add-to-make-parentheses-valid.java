bee95d0bba495cfc917ff61efd5d1ae5a0ef48d2
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-07-11 10:47:36 +0530

    Time: 2 ms (25.70%) | Memory: 41.3 MB (67.31%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> st = new Stack<>();
+        int ans = 0;
+        int openBrac = 0;
+        int closBrac = 0;
+        for(int i = 0; i < s.length(); i++){
+           if(s.charAt(i) == '('){
+            st.push(s.charAt(i));
+           } else {
+            if(st.isEmpty()){
+                ans++;
+            }
+             else if(st.peek() == '(' && s.charAt(i) == ')'){
+                st.pop();
+            }
+           }
+        }
+
+        if(st.isEmpty()){
+            return ans;
+        }
+
+        return ans + st.size();
+
+    }
+}
