76e30b48d33d3341fc73a449810ac887f64b6987
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-07-25 23:40:14 +0530

    Time: 24 ms (74.24%) | Memory: 57.3 MB (11.47%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,52 +1,49 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-       mergeSort(nums, 0, nums.length-1);
-       return nums;
-    }
-
-    static void merge(int[] arr, int si, int mid, int ei){
-        int[] temp = new int[ei - si + 1];
-
-        int i = si;
-        int j = mid+1;
-        int k = 0;
-
-        while(i <= mid && j <= ei){
-            if(arr[i] < arr[j]){
-                temp[k] = arr[i];
-                i++;
-            } else{
-                temp[k] = arr[j];
-                j++;
-            }
-            k++;
-        }
-
-        //if some are left
-        while(i <= mid){
-            temp[k++] = arr[i++];
-        }
-        while(j <= ei){
-            temp[k++] = arr[j++];
-        }
-
-        //add temp to arr
-        for(int l = 0, m = si; l < temp.length; m++, l++){
-            arr[m] = temp[l];
-        }
+        mergeSort(nums, 0, nums.length-1);
+        return nums;
     }
 
     static void mergeSort(int[] arr, int si, int ei){
         if(si >= ei){
             return;
         }
-
-        int mid = si+(ei-si)/2;
-        //left
+        int mid = si + (ei - si)/2;
+        //left part
         mergeSort(arr, si, mid);
-        //right
+        //right part
         mergeSort(arr, mid+1, ei);
-        //merge left+right
+        //merge
         merge(arr, si, mid, ei);
+    }
+
+    static void merge(int[] arr, int si, int mid, int ei){
+         int[] temp = new int[ei-si+1];
+
+         int i = si;
+         int j = mid+1;
+         int k = 0;
+
+         while(i <= mid && j <= ei){
+            if(arr[i] < arr[j]){
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+         }
+
+         while(i <= mid){
+            temp[k++] = arr[i++];
+         }
+         while(j <= ei){
+            temp[k++] = arr[j++];
+         }
+
+         for(i = si, k = 0; k < temp.length; i++, k++){
+            arr[i] = temp[k];
+         }
     }
 }
