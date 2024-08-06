bad990a79f12ab2850252e3f9bc4679ce9df74e6
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-08-07 00:14:44 +0530

    Time: 8 ms (91.92%) | Memory: 45.4 MB (91.35%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,36 +1,22 @@
 class Solution {
     public int minimumPushes(String word) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(int i = 0; i < word.length(); i++){
-            map.put(word.charAt(i), map.getOrDefault(word.charAt(i), 0)+1);
-        }
-        PriorityQueue<Pair> pq = new PriorityQueue<>((x,y) -> y.second - x.second);
-
-        for(Character key : map.keySet()){
-            pq.add(new Pair(key, map.get(key)));
-        }
-
         int ans = 0;
-        int count = 1;
-        int loopCount = 0;
-        while(!pq.isEmpty()){
-            ans += count * pq.remove().second;
-            loopCount++;
-            if(loopCount % 8 == 0){
+        int count = 0;
+        int[] freq = new int[26];
+
+        for(char c : word.toCharArray()){
+            freq[c-'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        for(int i = 25; i >= 0; i--){
+            if(freq[i] > 0){
+                ans += freq[i] * (count/8 + 1);
                 count++;
             }
         }
 
         return ans;
-    }
-}
-
-class Pair{
-    char first;
-    int second;
-
-    public Pair(char s, int i){
-        first = s;
-        second = i;
     }
 }
