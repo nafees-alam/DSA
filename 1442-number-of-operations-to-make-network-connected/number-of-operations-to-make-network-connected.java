e5b8e6ce86079f3434464d11ae18cda66d3654d6
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-08-06 19:44:40 +0530

    Time: 22 ms (13.72%) | Memory: 68.5 MB (24.88%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -0,0 +1,80 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        DisjointSet ds = new DisjointSet(n);
+        int extra = 0;
+        int l = connections.length;
+        for(int i = 0; i < l; i++){
+            int u = connections[i][0];
+            int v = connections[i][1];
+
+            if(ds.findUPar(u) == ds.findUPar(v)){
+                extra++;
+            } else {
+                ds.unionBySize(u, v);
+            }
+        }
+
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(ds.findUPar(i) == i){
+                count++;
+            }
+        }
+
+        int ans = count-1;
+        if(extra >= ans){
+            return ans;
+        }
+        return -1;
+    }
+}
+
+class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
+    }
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u);
+        } else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
+        }
+    }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
