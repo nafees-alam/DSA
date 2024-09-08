ad648d8e3790e7996caa7b5c7ab9620f92e7200f
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-09-09 00:13:21 +0530

    Time: 1 ms (99.84%) | Memory: 42.1 MB (72.13%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -2,41 +2,20 @@ class Solution {
     public String addBinary(String a, String b) {
         int carry = 0;
         StringBuilder sb = new StringBuilder();
-        int n = a.length();
-        int m = b.length();
-        char[] st2;
-        char[] st1;
-        if(m > n){
-            st2 = a.toCharArray();
-            st1 = b.toCharArray();
-        } else {
-            st1 = a.toCharArray();
-            st2 = b.toCharArray();
-        }
-        int k = st2.length-1;
-        for(int i = st1.length-1; i >= 0; i--){
-            int first = st1[i] - '0';
-            int second = 0;
-            if(k >= 0){
-                second = st2[k--] - '0';
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while(i >= 0 || j >= 0 || carry == 1){
+            if(i >= 0){
+                carry += a.charAt(i--) - '0';
             }
-            int sum = first + second + carry;
-            if(sum == 2){
-                sb.insert(0, 0);
-                carry = 1;
-            } else if(sum == 3){
-                sb.insert(0, 1);
-                carry = 1;
-            } else {
-                sb.insert(0, sum);
-                carry = 0;
+            if(j >= 0){
+                carry += b.charAt(j--) - '0';
             }
+            sb.append(carry % 2);
+            carry = carry/2;
         }
 
-        if(carry == 1){
-            sb.insert(0, 1);
-        }
-
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
