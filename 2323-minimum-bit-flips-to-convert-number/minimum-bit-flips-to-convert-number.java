d245a1c07fac5de3736fefffcbcea839ca7cb66f
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-09-11 12:11:00 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (64.36%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -1,16 +1,17 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        int count = 0;
-       while(start > 0 || goal > 0){
-           int n1 = start & 1;
-           int n2 = goal & 1;
-           if(n1 != n2){
-               count++;
-           }
-           start = start >> 1;
-           goal = goal >> 1;
-       }
+        int cnt = 0;
+        while(start > 0 || goal > 0){
+            int n1 = start & 1;
+            int n2 = goal & 1;
+            if(n1 != n2){
+                cnt++;
+            }
 
-       return count; 
+            start = start >> 1;
+            goal = goal >> 1;
+        }
+
+        return cnt;
     }
 }
