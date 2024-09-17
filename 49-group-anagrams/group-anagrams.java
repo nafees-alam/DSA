1a69092f6a583dce8f819976fab334cb8571cd29
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-09-17 17:51:08 +0530

    Time: 7 ms (65.26%) | Memory: 47.9 MB (42.98%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,47 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> list = new ArrayList<>();
-        List<String> str = new ArrayList<>(Arrays.asList(strs));
+        HashMap<String, List<String>> map = new HashMap<>();
 
-        while(str.size() > 0){
-            String curr = str.get(0);
-            str.remove(0);
-            List<String> temp = new ArrayList<>();
-            temp.add(curr);
-            for(int i = 0; i < str.size(); i++){
-                if(isAnagram(curr, str.get(i))){
-                    temp.add(str.get(i));
-                    str.remove(i);
-                    i--;
-                }
+        for(String word : strs){
+            char[] ch = word.toCharArray();
+            Arrays.sort(ch);
+            String anagram = new String(ch);
+
+            if(!map.containsKey(anagram)){
+                map.put(anagram, new ArrayList<>());
             }
-            if(temp.size() > 0){
-                list.add(temp);
-            }
-        }
-        return list;
-    }
 
-    boolean isAnagram(String s1, String s2){
-        if(s1.length() != s2.length()){
-            return false;
-        }
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(int i = 0; i < s1.length(); i++){
-            map.put(s1.charAt(i), map.getOrDefault(s1.charAt(i), 0) + 1);
+            map.get(anagram).add(word);
         }
 
-        for(int i = 0; i < s2.length(); i++){
-            if(!map.containsKey(s2.charAt(i))){
-                return false;
-            } else {
-                map.put(s2.charAt(i), map.get(s2.charAt(i))-1);
-                if(map.get(s2.charAt(i)) == 0){
-                    map.remove(s2.charAt(i));
-                }
-            }
-        }
-
-        return true;
+        return new ArrayList<>(map.values());
     }
 }
