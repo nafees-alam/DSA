efcad6c204b5b1d71b17abd5bfb1ab0d644560fb
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-09-22 12:44:31 +0530

    Time: 10 ms (23.25%) | Memory: 48.8 MB (5.32%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,11 +1,23 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length][amount+1];
-        for(int[] arr : dp){
-            Arrays.fill(arr, -1);
+        int[][] dp = new int[coins.length+1][amount+1];
+        for(int i = 0; i < dp.length; i++){
+            dp[i][0] = 1;
         }
 
-        return ways(coins, amount, coins.length-1, dp);
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 1; j < dp[0].length; j++){
+                int pick = 0;
+                if(coins[i-1] <= j){
+                    pick = dp[i][j - coins[i-1]];
+                }
+                int notPick = dp[i-1][j];
+
+                dp[i][j] = pick + notPick;
+            }
+        }
+
+        return dp[coins.length][amount];
     }
 
     int ways(int[] arr, int sum, int idx, int[][] dp){
@@ -22,7 +34,7 @@ class Solution {
         }
 
         int pick = ways(arr, sum - arr[idx], idx, dp);
-        
+
         int notPick = ways(arr, sum, idx - 1, dp);
 
         return dp[idx][sum] = pick + notPick;
