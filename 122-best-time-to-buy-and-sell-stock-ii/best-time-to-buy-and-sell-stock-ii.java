6b46937ca1c06acd7ee8e23ccf68c7dab6afce84
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-09-27 18:23:42 +0530

    Time: 1 ms (90.56%) | Memory: 45.7 MB (45.37%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][2];
-        for(int[] arr : dp){
-            Arrays.fill(arr, -1);
+        int n = prices.length;
+        int[][] dp = new int[n+1][2];
+
+        //base case
+        dp[n][1] = 0;
+        dp[n][0] = 0;
+
+        //loop for buy and idx
+        for(int i = n-1; i >= 0; i--){
+            for(int j = 1; j >= 0; j--){
+                if(j == 1){
+                    dp[i][j] = Math.max(-prices[i] + dp[i+1][0], dp[i+1][1]);
+                } else {
+                    dp[i][j] = Math.max(prices[i] + dp[i+1][1], dp[i+1][0]);
+                }
+            }
         }
 
-        return solve(prices, 0, 1, dp);
+        return dp[0][1];
     }
 
     public int solve(int[] arr, int idx, int buy, int[][] dp){
