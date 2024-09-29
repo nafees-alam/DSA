8743cf4bd0230fa8b725cdee2c628e86677e6c8c
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-09-29 15:39:26 +0530

    Time: 2 ms (11.88%) | Memory: 44.7 MB (83.96%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,28 +1,45 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        boolean[][] vis = new boolean[image.length][image[0].length];
+        if(image[sr][sc] == color){
+            return image;
+        }
 
-        helper(image, sr, sc, color, vis, image[sr][sc]);
+        int m = image.length;
+        int n = image[0].length;
+
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(sr, sc));
+        int initialColor = image[sr][sc];
+        image[sr][sc] = color;
+
+        while(!q.isEmpty()){
+            int row = q.peek().first;
+            int col = q.peek().second;
+            q.remove();
+
+            for(int i = 0; i < 4; i++){
+                int nrow = row + dr[i];
+                int ncol = col + dc[i];
+
+                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && image[nrow][ncol] == initialColor){
+                    q.add(new Pair(nrow, ncol));
+                    image[nrow][ncol] = color;
+                }
+            }
+        }
+
         return image;
     }
+}
 
-    static void helper(int[][] image, int sr, int sc, int color, boolean[][] vis, int orgCol){
-        //base case
-        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length
-           || vis[sr][sc] || image[sr][sc] != orgCol){
-               return;
-           }
+class Pair{
+    int first;
+    int second;
 
-        image[sr][sc] = color;
-        vis[sr][sc] = true;
-
-        //left
-        helper(image, sr, sc-1, color, vis, orgCol);
-        //right
-        helper(image, sr, sc+1, color, vis, orgCol);
-        //up
-        helper(image, sr-1, sc, color, vis, orgCol);
-        //down
-        helper(image, sr+1, sc, color, vis, orgCol);
+    public Pair(int i, int j){
+        first = i;
+        second = j;
     }
 }
