9ed564cc67f53fba65f9b3a408a3668de931df6e
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-10-05 09:50:54 +0530

    Time: 13 ms (90.50%) | Memory: 57.4 MB (79.75%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -1,46 +1,22 @@
 class Solution {
     public long dividePlayers(int[] skill) {
-        long totSum = 0;
+        long ans = 0;
         int n = skill.length;
-        for(int i = 0; i < n; i++){
-            totSum += skill[i];
-        }
-
-        if(n/2 % 2 == 0 && totSum % 2 != 0){
-            return -1;
-        }
 
         Arrays.sort(skill);
 
-        int reqSum = (int)totSum/(n/2);
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < n; i++){
-            map.put(skill[i], map.getOrDefault(skill[i], 0) + 1);
-        }
+        int i = 0;
+        int j = n-1;
+        int reqSum = skill[0] + skill[n-1];
 
-        for(int key : map.keySet()){
-            System.out.println(key + "->" + map.get(key));
-        }
-
-        System.out.println(reqSum);
-
-        long ans = 0;
-        
-        for(int i = 0; i < n/2; i++){
-            int key = skill[i];
-            if(!map.containsKey(reqSum-key)){
+        while(i < j){
+            if(skill[i] + skill[j] != reqSum){
                 return -1;
             }
 
-            ans += key * (reqSum - key);
-            map.put(key, map.get(key) - 1);
-            map.put(reqSum-key, map.get(reqSum-key)-1);
-            if(map.get(key) == 0){
-                map.remove(key);
-            }
-            if(map.containsKey(reqSum-key) && map.get(reqSum - key) == 0){
-                map.remove(reqSum - key);
-            }
+            ans += skill[i] * skill[j];
+            i++;
+            j--;
         }
 
         return ans;
