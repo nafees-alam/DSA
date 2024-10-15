2d0f437808e5bb168f9c54d964081c8fb81d641b
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-10-15 11:29:34 +0530

    Time: 7 ms (100.00%) | Memory: 44.9 MB (97.85%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -1,32 +1,17 @@
 class Solution {
     public long minimumSteps(String s) {
         char[] arr = s.toCharArray();
-        int swapIdx = -1;
+        int swapCnt = 0;
         long ans = 0;
 
         for(int i = 0; i < arr.length; i++){
-            if(arr[i] == '1'){
-                swapIdx = i;
-                break;
-            }
-        }
-
-        for(int i = 0, j = 1; i < arr.length && j < arr.length; i++, j++){
-            if(arr[i] == '1' && arr[j] == '0'){
-                slide(arr, i, j);
-                ans += j - swapIdx;
-                swapIdx++;
+            if(arr[i] == '0'){
+                ans += swapCnt;
+            } else {
+                swapCnt++;
             }
         }
 
         return ans;
-    }
-
-    void slide(char[] arr, int i, int j){
-        char temp = arr[j];
-        for(int k = j; k > i; k--) {
-            arr[k] = arr[k - 1];
-        }
-        arr[i] = temp;
     }
 }
