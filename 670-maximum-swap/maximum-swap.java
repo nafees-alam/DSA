5625143a1c69cfea58846dd5191079023e017a08
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2024-10-17 16:00:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (66.95%) - LeetSync

--- 670-maximum-swap/maximum-swap.java
+++ 670-maximum-swap/maximum-swap.java
@@ -1,35 +1,33 @@
 class Solution {
     public int maximumSwap(int num) {
-        int[] arr = Arrays.stream(String.valueOf(num).split(""))
-                     .mapToInt(Integer::parseInt) 
-                     .toArray();
-        
-        int[] maxRight = new int[arr.length];
-        maxRight[arr.length-1] = arr.length-1;
-        for(int i = arr.length-2; i >= 0; i--){
-            if(arr[i] <= arr[maxRight[i+1]]){
-                maxRight[i] = maxRight[i+1];
-            } else{
-                maxRight[i] = i;
+        String str = Integer.toString(num);
+        int[] maxRight = new int[10];
+        Arrays.fill(maxRight, -1);
+
+        for(int i = 0; i < str.length(); i++){
+            int dig = str.charAt(i)-'0';
+            maxRight[dig] = i;
+        }
+
+        for(int i = 0; i < str.length(); i++){
+            int currDig = str.charAt(i)-'0';
+            for(int j = 9; j > currDig; j--){
+                if(i < maxRight[j]){
+                    str = swap(str, i, maxRight[j]);
+                    return Integer.parseInt(str);
+                }
             }
         }
 
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] < arr[maxRight[i]]){
-                int temp = arr[i];
-                arr[i] = arr[maxRight[i]];
-                arr[maxRight[i]] = temp;
-                break;
-            }
-        }
+        return num;
+    }
 
-        int ans = 0;
-        int exp = arr.length-1;
-        for(int k = 0; k < arr.length; k++){
-            ans += Math.pow(10, exp) * arr[k];
-            exp--;
-        }
+    String swap(String s, int i, int j){
+        char[] ch = s.toCharArray();
+        char temp = ch[i];
+        ch[i] = ch[j];
+        ch[j] = temp;
 
-        return ans;
+        return new String(ch);
     }
 }
