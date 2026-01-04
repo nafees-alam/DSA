919c50b8cd93ea8dd2a1969ef69e64060f856f77
nafees-alam <123291019+nafees-alam@users.noreply.github.com>
2026-01-04 20:09:54 +0530

    Time: 2 ms (61.34%) | Memory: 46.5 MB (26.18%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,29 +15,30 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < inorder.length; i++){
-            map.put(inorder[i] , i);
+            map.put(inorder[i], i);
         }
 
-        TreeNode root = helper(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1, map);
-
-        return root;
+        return helper(preorder, 0, preorder.length - 1, inorder, 0, inorder.length-1, map);
     }
 
-    static TreeNode helper(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd, HashMap<Integer, Integer> map){
-        if(preStart > preEnd || inStart > inEnd){
-            return null;
-        }
+    public TreeNode helper(int[] preOrder, int preStart, int preEnd, int[] inOrder, 
+                            int inStart, int inEnd, Map<Integer, Integer> map){
+            
+            if(preStart > preEnd || inStart > inEnd){
+                return null;
+            }
 
-        TreeNode root = new TreeNode(preorder[preStart]);
-        int inRoot = map.get(root.val);
-        int numsLeft = inRoot - inStart;
+            TreeNode root = new TreeNode(preOrder[preStart]);
+            int inRoot = map.get(root.val);
+            int numsLeft = inRoot - inStart;
 
-        root.left = helper(preorder, preStart+1, preStart + numsLeft, inorder, inStart, inRoot -1, map);
-        root.right = helper(preorder, preStart+numsLeft+1, preEnd, inorder, inRoot+1, inEnd, map);
+            //left Call
+            root.left = helper(preOrder, preStart + 1, preStart + numsLeft, inOrder,        inStart, inRoot -1, map);
+            //right call
+            root.right = helper(preOrder, preStart+numsLeft + 1, preEnd, inOrder, inRoot+1, inEnd, map);
 
-        return root;
+            return root;
     }
 }
